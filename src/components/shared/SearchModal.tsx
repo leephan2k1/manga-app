7e0bfb7d8c3b9b6556cab1084b24ecb070ac05b5
leephan <pttl2k1@gmail.com>
2022-05-31 17:53:31 +0700

    style: 💄 search modal

--- src/components/shared/SearchModal.tsx
+++ src/components/shared/SearchModal.tsx
@@ -1,13 +1,29 @@
 import { Dialog, Transition } from '@headlessui/react';
-import { Fragment, useRef } from 'react';
+import { Fragment, useRef, useState } from 'react';
 import { XIcon } from '@heroicons/react/outline';
+import { SearchIcon } from '@heroicons/react/solid';
 import { useRecoilState } from 'recoil';
 import { searchModalState } from '~/atoms/searchModelAtom';
-import { SearchIcon } from '@heroicons/react/solid';
+import SearchResult from './SearchResult';
 
 export default function SearchModal() {
     const [showModal, setShowModal] = useRecoilState(searchModalState);
+    const [showBtnClearInput, setShowBtnClearInput] = useState(false);
     const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleClearSearchValue = () => {
+        if (inputRef.current) {
+            inputRef.current.value = '';
+        }
+        setShowBtnClearInput(false);
+    };
+
+    const handleOpenButtonClearSearch = () => {
+        //Displayed only when input has value
+        if (inputRef.current?.value) {
+            setShowBtnClearInput(true);
+        }
+    };
 
     return (
         <Transition appear show={showModal} as={Fragment}>
@@ -40,7 +56,7 @@ export default function SearchModal() {
                             leaveFrom="opacity-100 scale-100"
                             leaveTo="opacity-0 scale-95"
                         >
-                            <Dialog.Panel className="h-fit w-[85%] transform overflow-hidden rounded-2xl bg-background p-6 text-left align-middle shadow-xl transition-all md:w-[75%]">
+                            <Dialog.Panel className="max-h-[70vh] w-[85%] transform overflow-x-hidden overflow-y-scroll rounded-2xl bg-background p-6 text-left align-middle shadow-xl transition-all md:w-[75%] lg:max-h-[85vh]">
                                 <div className="flex items-center justify-between">
                                     <Dialog.Title
                                         as="h3"
@@ -56,13 +72,29 @@ export default function SearchModal() {
                                     </button>
                                 </div>
                                 <div className="my-10 flex h-[60px] items-center rounded-xl bg-secondary py-4 text-white">
+                                    {/* search icon  */}
                                     <SearchIcon className="mx-2 h-10 w-10 md:mx-6 md:h-14 md:w-14" />
+                                    {/* search input  */}
                                     <input
                                         ref={inputRef}
                                         type="text"
                                         className="w-full bg-transparent p-4"
+                                        onBlur={handleOpenButtonClearSearch}
+                                        onFocus={() =>
+                                            setShowBtnClearInput(false)
+                                        }
                                     />
+                                    {/* clear input  */}
+                                    {showBtnClearInput && (
+                                        <button
+                                            className="absolute-center m-4 h-10 w-10 rounded-lg bg-primary text-white hover:opacity-60 md:h-14 md:w-14 md:rounded-xl"
+                                            onClick={handleClearSearchValue}
+                                        >
+                                            <XIcon className="h-8 w-8" />
+                                        </button>
+                                    )}
                                 </div>
+                                <SearchResult />
                             </Dialog.Panel>
                         </Transition.Child>
                     </div>
