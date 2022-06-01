0d40508f4e1cef098d47bb57b9210595632f7d43
leephan <pttl2k1@gmail.com>
2022-06-01 15:41:21 +0700

    fix: 🐛 dynamic import resolve "Expected server HTML..."

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,14 +1,16 @@
+import dynamic from 'next/dynamic';
 import { ReactNode } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
 
 import Footer from '../partials/Footer';
 import Header from '../partials/Header';
-import Sidebar from '../partials/Sidebar';
-import SearchModal from '../shared/SearchModal';
 
 interface MainLayoutProps {
     children: ReactNode;
 }
+
+const Sidebar = dynamic(() => import('../partials/Sidebar'));
+const SearchModal = dynamic(() => import('../shared/SearchModal'));
 
 export default function MainLayout({ children }: MainLayoutProps) {
     const matches = useMediaQuery('(max-width: 1024px)');
@@ -18,7 +20,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
             <Header />
             {matches && <Sidebar />}
             <SearchModal />
-            {children}
+            <main>{children}</main>
             <Footer />
         </>
     );
