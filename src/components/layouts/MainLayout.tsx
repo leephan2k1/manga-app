3b91c76b59a79eb848ba59130ee28c2383c4e3cc
leephan2k1 <pttl2k1@gmail.com>
2022-06-06 01:21:21 +0700

    perf: ⚡️ ShowModal doesn't need to render the first time

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,6 +1,8 @@
 import dynamic from 'next/dynamic';
 import { ReactNode } from 'react';
+import { useRecoilValue } from 'recoil';
 import { useMediaQuery } from 'usehooks-ts';
+import { searchModalState } from '~/atoms/searchModelAtom';
 
 import Footer from '../partials/Footer';
 import Header from '../partials/Header';
@@ -14,12 +16,13 @@ const SearchModal = dynamic(() => import('../shared/SearchModal'));
 
 export default function MainLayout({ children }: MainLayoutProps) {
     const matches = useMediaQuery('(max-width: 1024px)');
+    const showModal = useRecoilValue(searchModalState);
 
     return (
         <>
             <Header />
             {matches && <Sidebar />}
-            <SearchModal />
+            {showModal && <SearchModal />}
             <main>{children}</main>
             <Footer />
         </>
