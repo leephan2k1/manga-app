a210d4f88ebbf0bc57e576625321f847bfb36c94
leephan2k1 <pttl2k1@gmail.com>
2022-06-09 21:34:15 +0700

    feat: 🎸 add tooltip chapter title on desktop

--- src/components/shared/DetailsChapterList.tsx
+++ src/components/shared/DetailsChapterList.tsx
@@ -1,6 +1,9 @@
+import 'react-tippy/dist/tippy.css';
+
 import Link from 'next/link';
-import { ChapterList } from '~/types';
 import { memo } from 'react';
+import { Tooltip } from 'react-tippy';
+import { ChapterList } from '~/types';
 
 import { BookOpenIcon, DocumentTextIcon } from '@heroicons/react/solid';
 
@@ -34,30 +37,42 @@ function DetailsChapterList({
                     chapterList.map((chapter, idx) => {
                         return (
                             <li key={chapter.chapterId || idx}>
-                                <Link href="">
-                                    <a
-                                        className={`${
-                                            !mobileUI &&
-                                            'bubble-top-left-to-bottom-right'
-                                        }  flex h-[30px] items-center justify-between rounded-lg bg-deep-black  md:h-[100px] md:flex-col md:items-start md:justify-center md:space-y-4`}
-                                    >
-                                        <div className="flex w-[30%] min-w-max items-center md:w-full md:flex-row-reverse md:justify-between md:px-4">
-                                            {mobileUI ? (
-                                                <DocumentTextIcon className="mx-4 h-4 w-4" />
-                                            ) : (
-                                                <BookOpenIcon className="mx-4 md:h-10 md:w-10 lg:h-10 lg:w-14" />
-                                            )}
-                                            <span className="text-lg font-bold line-clamp-1 hover:text-white md:max-w-[100px] md:text-2xl">
-                                                {chapter.chapterTitle}
-                                            </span>
-                                        </div>
-                                        <div className="flex items-center px-4 md:w-full">
-                                            <span className="text-lg font-extralight text-gray-300 md:text-2xl">
-                                                {chapter.updatedAt}
-                                            </span>
-                                        </div>
-                                    </a>
-                                </Link>
+                                <Tooltip
+                                    // options
+                                    disabled={mobileUI}
+                                    title={chapter.chapterTitle}
+                                    position="bottom"
+                                    followCursor={true}
+                                    size={'big'}
+                                    theme={'light'}
+                                    animation={'none'}
+                                >
+                                    <Link href="/">
+                                        <a
+                                            className={`${
+                                                !mobileUI &&
+                                                'bubble-top-left-to-bottom-right'
+                                            }  flex h-[30px] items-center justify-between rounded-lg bg-deep-black  md:h-[100px] md:flex-col md:items-start md:justify-center md:space-y-4`}
+                                        >
+                                            <div className="flex w-[30%] min-w-max items-center    md:justify-between md:px-4">
+                                                {mobileUI && (
+                                                    <DocumentTextIcon className="mx-4 h-4 w-4" />
+                                                )}
+                                                <span className="max-w-[200px] text-lg font-bold line-clamp-1 hover:text-white md:max-w-[140px] md:text-xl  lg:max-w-[160px] lg:text-2xl">
+                                                    {chapter.chapterTitle}
+                                                </span>
+                                            </div>
+                                            <div className="flex items-center px-4 md:w-full md:justify-between">
+                                                <span className="text-lg font-extralight text-gray-300 md:text-2xl">
+                                                    {chapter.updatedAt}
+                                                </span>
+                                                {!mobileUI && (
+                                                    <BookOpenIcon className="lg:min-w-14 md:h-10 md:w-10 lg:h-10 lg:w-14" />
+                                                )}
+                                            </div>
+                                        </a>
+                                    </Link>
+                                </Tooltip>
                             </li>
                         );
                     })}
