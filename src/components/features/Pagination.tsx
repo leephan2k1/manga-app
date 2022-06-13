21230f34120816bc2a15f68e382644fb057a9266
leephan2k1 <pttl2k1@gmail.com>
2022-06-13 21:58:25 +0700

    feat: 🎸 pagination

---
+++ src/components/features/Pagination.tsx
@@ -0,0 +1,105 @@
+import { MouseEvent, useEffect, useState } from 'react';
+import PageInput from '../shared/PageInput';
+import usePushQuery from '~/hooks/usePushQuery';
+import { useRouter } from 'next/router';
+
+export default function Pagination() {
+    const [query] = usePushQuery();
+    const router = useRouter();
+    const totalPages = 313;
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const handleChangePage = (e: MouseEvent<HTMLLIElement>) => {
+        setCurrentPage(Number(e.currentTarget.dataset.id));
+        query.push('page', String(e.currentTarget.dataset.id));
+    };
+
+    const handleInputPage = (page: number) => {
+        if (page > totalPages) {
+            setCurrentPage(totalPages);
+            query.push('page', String(totalPages));
+            return;
+        }
+
+        if (page < 1) {
+            setCurrentPage(1);
+            query.push('page', String(1));
+            return;
+        }
+
+        setCurrentPage(page);
+        query.push('page', String(page));
+    };
+
+    useEffect(() => {
+        const { page } = router.query;
+        setCurrentPage(page ? Number(page) : 1);
+    }, [router.query]);
+
+    return (
+        <div className="absolute-center min-h-[50px] w-full bg-cyan-400/0">
+            <ul className="flex h-full w-full flex-wrap items-center justify-center gap-4 text-3xl text-white">
+                <li
+                    onClick={handleChangePage}
+                    data-id={1}
+                    className={`pagination-active rounded-lg ${
+                        currentPage === 1
+                            ? 'bg-primary hover:bg-primary/60'
+                            : 'bg-highlight hover:bg-highlight/25'
+                    } py-4 px-7 font-secondary  transition-all hover:cursor-pointer `}
+                >
+                    1
+                </li>
+
+                {currentPage > 4 && (
+                    <PageInput
+                        setCurrentPage={handleInputPage}
+                        totalPages={totalPages}
+                    />
+                )}
+
+                {[
+                    currentPage - 2,
+                    currentPage - 1,
+                    currentPage,
+                    currentPage + 1,
+                    currentPage + 2,
+                ].map((number) => {
+                    if (number > 1 && number < totalPages)
+                        return (
+                            <li
+                                onClick={handleChangePage}
+                                data-id={number}
+                                className={`pagination-active rounded-lg ${
+                                    number === currentPage
+                                        ? 'bg-primary hover:bg-primary/60'
+                                        : 'bg-highlight hover:bg-highlight/25'
+                                } py-4 px-7 font-secondary  transition-all hover:cursor-pointer hover:bg-highlight/25`}
+                            >
+                                {number}
+                            </li>
+                        );
+                })}
+
+                {totalPages - currentPage > 3 && (
+                    <PageInput
+                        setCurrentPage={handleInputPage}
+                        totalPages={totalPages}
+                    />
+                )}
+
+                <li
+                    onClick={handleChangePage}
+                    data-id={totalPages}
+                    className={`pagination-active rounded-lg ${
+                        currentPage === totalPages
+                            ? 'bg-primary hover:bg-primary/60'
+                            : 'bg-highlight hover:bg-highlight/25'
+                    } py-4 px-7 font-secondary  transition-all hover:cursor-pointer hover:bg-highlight/25`}
+                >
+                    {totalPages}
+                </li>
+            </ul>
+        </div>
+    );
+}
---
+++ src/components/shared/PageInput.tsx
@@ -0,0 +1,49 @@
+import { KeyboardEvent, memo, useRef, useState } from 'react';
+import { useOnClickOutside } from 'usehooks-ts';
+
+import { DotsHorizontalIcon } from '@heroicons/react/outline';
+
+interface PageInputProps {
+    totalPages: number;
+    setCurrentPage: (page: number) => void;
+}
+
+function PageInput({ totalPages, setCurrentPage }: PageInputProps) {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const [showInput, setShowInput] = useState(false);
+
+    const handleNavigatePage = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            setCurrentPage(Number(e.currentTarget.value));
+            setShowInput(false);
+        }
+    };
+
+    useOnClickOutside(inputRef, () => setShowInput(false));
+
+    if (showInput) {
+        return (
+            <input
+                ref={inputRef}
+                autoFocus
+                className="h-14 w-20 rounded-lg border-[1px] border-white bg-transparent p-4"
+                onKeyUp={handleNavigatePage}
+                type={'number'}
+                min={1}
+                max={totalPages}
+            />
+        );
+    } else {
+        return (
+            <button
+                onClick={() => setShowInput(true)}
+                className="rounded-lg p-4 hover:bg-highlight"
+            >
+                <DotsHorizontalIcon className="h-8 w-8" />
+            </button>
+        );
+    }
+}
+
+export default memo(PageInput);
