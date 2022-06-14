83dda67db2691d9374e2c6538554127ed27fccc5
leephan2k1 <pttl2k1@gmail.com>
2022-06-14 17:47:14 +0700

    feat: 🎸 Virtualization list for mobile

--- src/components/shared/DetailsChapterList.tsx
+++ src/components/shared/DetailsChapterList.tsx
@@ -3,6 +3,7 @@ import 'tippy.js/dist/tippy.css';
 import Link from 'next/link';
 import { memo, useEffect, useState } from 'react';
 import LazyLoad, { forceVisible } from 'react-lazyload';
+import { Virtuoso } from 'react-virtuoso';
 import { animateFill, followCursor } from 'tippy.js';
 import { ChapterList } from '~/types';
 
@@ -55,60 +56,88 @@ function DetailsChapterList({
             </div>
 
             {/* chapter list  */}
-            <ul className="z-0 my-4 flex  h-[500px] w-full flex-col  gap-2  overflow-y-scroll px-4 text-white  md:grid md:h-fit md:grid-cols-4  md:overflow-y-hidden lg:grid-cols-7">
-                {list &&
-                    list.length &&
-                    list.map((chapter, idx) => {
-                        return (
-                            <LazyLoad
-                                key={chapter.chapterId || idx}
-                                overflow={mobileUI}
-                                offset={mobileUI ? 0 : 200}
-                            >
-                                <li className="animate__fadeIn animate__animated">
-                                    <LazyTippy
-                                        disabled={mobileUI}
-                                        content={chapter.chapterTitle}
-                                        interactiveBorder={20}
-                                        followCursor={true}
-                                        animateFill={true}
-                                        plugins={[followCursor, animateFill]}
+            {chapterList && chapterList.length && mobileUI ? (
+                <Virtuoso
+                    style={{ height: '600px' }}
+                    totalCount={chapterList.length}
+                    itemContent={(index) => (
+                        <div className="animate__fadeIn animate__animated m-2 text-white">
+                            <button className="h-full w-full">
+                                <Link href="/">
+                                    <a
+                                        className={`flex h-[30px] items-center justify-between rounded-lg bg-deep-black  md:h-[100px] md:flex-col md:items-start md:justify-center md:space-y-4`}
                                     >
-                                        <button className="h-full w-full">
-                                            <Link href="/">
-                                                <a
-                                                    className={`${
-                                                        !mobileUI &&
-                                                        'bubble-top-left-to-bottom-right'
-                                                    }  flex h-[30px] items-center justify-between rounded-lg bg-deep-black  md:h-[100px] md:flex-col md:items-start md:justify-center md:space-y-4`}
-                                                >
-                                                    <div className="flex w-[30%] min-w-max items-center    md:justify-between md:px-4">
-                                                        {mobileUI && (
-                                                            <DocumentTextIcon className="mx-4 h-4 w-4" />
-                                                        )}
-                                                        <span className="max-w-[200px] text-lg font-bold line-clamp-1 hover:text-white md:max-w-[140px] md:text-xl  lg:max-w-[160px] lg:text-2xl">
-                                                            {
-                                                                chapter.chapterTitle
-                                                            }
-                                                        </span>
-                                                    </div>
-                                                    <div className="flex items-center px-4 md:w-full md:justify-between">
-                                                        <span className="text-lg font-extralight text-gray-300 md:text-2xl">
-                                                            {chapter.updatedAt}
-                                                        </span>
-                                                        {!mobileUI && (
+                                        <div className="flex w-[30%] min-w-max items-center    md:justify-between md:px-4">
+                                            <DocumentTextIcon className="mx-4 h-4 w-4" />
+
+                                            <span className="max-w-[200px] text-lg font-bold line-clamp-1 hover:text-white md:max-w-[140px] md:text-xl  lg:max-w-[160px] lg:text-2xl">
+                                                {
+                                                    chapterList[index]
+                                                        ?.chapterTitle
+                                                }
+                                            </span>
+                                        </div>
+                                        <div className="flex items-center px-4 md:w-full md:justify-between">
+                                            <span className="text-lg font-extralight text-gray-300 md:text-2xl">
+                                                {chapterList[index]?.updatedAt}
+                                            </span>
+                                        </div>
+                                    </a>
+                                </Link>
+                            </button>
+                        </div>
+                    )}
+                />
+            ) : (
+                <ul className="z-0 my-4 flex  h-[500px] w-full flex-col  gap-2  overflow-y-scroll px-4 text-white  md:grid md:h-fit md:grid-cols-4  md:overflow-y-hidden lg:grid-cols-7">
+                    {list &&
+                        list.length &&
+                        list.map((chapter, idx) => {
+                            return (
+                                <LazyLoad key={chapter.chapterId || idx}>
+                                    <li className="animate__fadeIn animate__animated">
+                                        <LazyTippy
+                                            disabled={mobileUI}
+                                            content={chapter.chapterTitle}
+                                            interactiveBorder={20}
+                                            followCursor={true}
+                                            animateFill={true}
+                                            plugins={[
+                                                followCursor,
+                                                animateFill,
+                                            ]}
+                                        >
+                                            <button className="h-full w-full">
+                                                <Link href="/">
+                                                    <a
+                                                        className={`bubble-top-left-to-bottom-right
+                                                           flex h-[30px] items-center justify-between rounded-lg bg-deep-black  md:h-[100px] md:flex-col md:items-start md:justify-center md:space-y-4`}
+                                                    >
+                                                        <div className="flex w-[30%] min-w-max items-center    md:justify-between md:px-4">
+                                                            <span className="max-w-[200px] text-lg font-bold line-clamp-1 hover:text-white md:max-w-[140px] md:text-xl  lg:max-w-[160px] lg:text-2xl">
+                                                                {
+                                                                    chapter.chapterTitle
+                                                                }
+                                                            </span>
+                                                        </div>
+                                                        <div className="flex items-center px-4 md:w-full md:justify-between">
+                                                            <span className="text-lg font-extralight text-gray-300 md:text-2xl">
+                                                                {
+                                                                    chapter.updatedAt
+                                                                }
+                                                            </span>
                                                             <BookOpenIcon className="lg:min-w-14 md:h-10 md:w-10 lg:h-10 lg:w-14" />
-                                                        )}
-                                                    </div>
-                                                </a>
-                                            </Link>
-                                        </button>
-                                    </LazyTippy>
-                                </li>
-                            </LazyLoad>
-                        );
-                    })}
-            </ul>
+                                                        </div>
+                                                    </a>
+                                                </Link>
+                                            </button>
+                                        </LazyTippy>
+                                    </li>
+                                </LazyLoad>
+                            );
+                        })}
+                </ul>
+            )}
         </div>
     );
 }
