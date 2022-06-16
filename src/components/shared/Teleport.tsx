89ddae2e9ca15dfbf63e0158559e344321607989
leephan2k1 <pttl2k1@gmail.com>
2022-06-16 22:49:09 +0700

    feat: 🎸 teleport component to outside root

---
+++ src/components/shared/Teleport.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+interface PortalProps {
+    children: React.ReactNode;
+    selector?: string;
+}
+
+const Portal: React.FC<PortalProps> = ({ children, selector = 'body' }) => {
+    const [el, setEl] = useState<Element | null>(null);
+
+    useEffect(() => {
+        setEl(document.querySelector(selector));
+    }, [selector]);
+
+    return el ? ReactDOM.createPortal(children, el) : null;
+};
+
+export default React.memo(Portal);
