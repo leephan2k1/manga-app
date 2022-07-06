b8c9e3ca2e0481909aeb7bb32662ef7c1ac17256
leephan2k1 <pttl2k1@gmail.com>
2022-07-06 17:07:48 +0700

    fix: 🐛 check array before use

--- src/utils/webtoonChecker.ts
+++ src/utils/webtoonChecker.ts
@@ -1,10 +1,11 @@
 import { MangaDetails } from '~/types';
 
 export default function webtoonChecker(manga: MangaDetails) {
-    return manga?.genres?.some(
-        (genre) =>
-            !!['manhua', 'manhwa', 'webtoon'].find(
-                (i) => i === genre.genreTitle.trim().toLocaleLowerCase(),
-            ),
-    );
+    if (Array.isArray(manga.genres))
+        return manga?.genres?.some(
+            (genre) =>
+                !!['manhua', 'manhwa', 'webtoon'].find(
+                    (i) => i === genre.genreTitle.trim().toLocaleLowerCase(),
+                ),
+        );
 }
